33647c0f32b591562e31b4e37ee6a384249377c3
hourin <phucjaki@gmail.com>
2026-05-29 16:52:02 +0700

    Added: running ner.py to analyze repository

--- trol.py
+++ trol.py
@@ -1,4 +1,24 @@
 import streamlit as st;
+import subprocess;
+import os;
+
+def execute(url) -> str:
+    orig_dir = os.getcwd();
+    ner_path = "/mnt/d/pbl5-queries/";
+
+    try:
+        os.chdir(ner_path);
+        result = subprocess.run(
+            ["uv", "run", "ner.py", "-r", url],
+            capture_output=True,
+            text=True,
+            check=True,
+        );
+
+        with open("csv.csv", "r") as file:
+            return file.read();
+    finally:
+        os.chdir(orig_dir);
 
 st.title("lingang guliguli");
 
@@ -6,4 +26,5 @@ url = st.text_input("link");
 
 if st.checkbox("show shit"):
     st.write(f"actual {url} shit");
+    st.write(execute(url));
 
