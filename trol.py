a4e94a4bef21dc2c07d9ffac1fcd931778e1b304
hourin <phucjaki@gmail.com>
2026-05-29 11:15:18 +0700

    Added: testing text input

--- trol.py
+++ trol.py
@@ -2,6 +2,8 @@ import streamlit as st;
 
 st.title("lingang guliguli");
 
+url = st.text_input("link");
+
 if st.checkbox("show shit"):
-    st.write("actual shit");
+    st.write(f"actual {url} shit");
 
